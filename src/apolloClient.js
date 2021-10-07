1c888684218c80456024d559f06ba6b32de5c017
krisjanis-a <krishjanis96@gmail.com>
2021-10-07 15:16:30 +0300

    changed apollo client cache settings

--- src/apolloClient.js
+++ src/apolloClient.js
@@ -2,9 +2,7 @@ import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
 
 const apolloClient = new ApolloClient({
   uri: "http://localhost:4000/",
-  cache: new InMemoryCache({
-    resultCaching: false,
-  }),
+  cache: new InMemoryCache(),
 });
 
 const makeQuery = async (queryObject) => {
